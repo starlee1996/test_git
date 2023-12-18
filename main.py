5661c0462a94266441052a96eb531bd4bfd42206
Yixin Li <liyixin092100@163.com>
2023-12-18 16:10:19 +0800

    new print content

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('hello world')
+    print('new world')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
